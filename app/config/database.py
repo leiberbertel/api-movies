3f1fe116d4adca25a1d7bafc2124d58c5cf032c3
Leiber Bertel <leiberbertel777@gmail.com>
2023-03-02 22:35:53 -0500

    add folder config: file database, hashing, oauth and settings

---
+++ app/config/database.py
@@ -0,0 +1,33 @@
+"""Database configuration."""
+
+import os
+from typing import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+sqlite_filename = "../../database.sqlite"
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(base_dir, sqlite_filename)}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
+)
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db() -> Generator:
+    """Get a database session.
+
+    Yields:
+        Session: The database session.
+    """
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
