cde2ecbace6178ec19635e054b39dd747f1940e4
Leiber Bertel <leiberbertel777@gmail.com>
2023-03-02 22:40:23 -0500

    add folder: movie schema file

---
+++ app/schemas/movie.py
@@ -0,0 +1,37 @@
+"""Movie Pydantic model for API."""
+
+from typing import Union
+
+from pydantic import BaseModel, Field
+
+
+class Movie(BaseModel):
+    """Movie Pydantic model.
+
+    Attributes:
+        title (str): Movie title.
+        overview (str): Movie overview.
+        year (int): Movie year.
+        rating (int): Movie rating.
+        genre (str): Movie genre.
+
+    """
+
+    title: str = Field(..., max_length=100)
+    overview: str = Field(..., max_length=500)
+    year: int = Field(..., gt=1900, lt=2022)
+    rating: Union[int, float] = Field(..., gt=0.0, lt=10.0)
+    genre: str = Field(..., max_length=100)
+
+    class Config:
+        """Pydantic config."""
+
+        schema_extra = {
+            "example": {
+                "title": "The Shawshank Redemption",
+                "overview": "Two imprisoned",
+                "year": 1994,
+                "rating": 9.3,
+                "genre": "Drama",
+            }
+        }
